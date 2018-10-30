f5416de8666ace2b876e86d7448cf3a1228fc890
Beau Spencer <bdspencer416@gmail.com>
2018-10-29 20:58:10 -0700

    set up ionic project and built basic form component to gather users email and password for facebook. #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ReactiveFormsModule }   from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,6 +13,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
