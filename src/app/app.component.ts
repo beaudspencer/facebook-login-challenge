11fe02f54c0d0abd9dbcfd48038e69715790e4e8
Beau Spencer <bdspencer416@gmail.com>
2018-10-30 07:25:22 -0700

    Rewrote to use FB login method after properly installing the facebook sdk in the component constructor. redid html and added some basic styling via the scss file. #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,80 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, Button } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
-import { FormControl, FormGroup } from '@angular/forms';
+
+declare const window:any;
+declare const FB:any
 
 @Component({
   templateUrl: 'app.html'
 })
+
 export class MyApp {
   rootPage:any = HomePage;
-  login = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl('')
-  });
+  onClick() {
+    FB.getLoginStatus(res => {
+      if (res.status !== 'connected') {
+        this.logIn()
+      }
+    })
+  }
+  logIn() {
+    const loginButton = document.querySelector('button')
+    FB.login(() => {
+      loginButton.disabled = true
+      this.grabData()
+    })
+  }
+  grabData() {
+    const user = document.createElement('p')
+    const photo = document.createElement('img')
+    const container = document.querySelector('#log_container')
+    user.id = 'user'
+    photo.id = 'photo'
+    FB.api('/me', res => {
+      user.textContent = res.name
+      container.appendChild(user)
+      FB.api(
+        `/${res.id}/picture`,
+        'GET',
+        {"redirect": "false"},
+        response => {
+          photo.src = response.data.url
+          container.appendChild(photo)
+        }
+      )
+    })
+  }
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    // FaceBook SDK boilerplate code for install and initialization
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0]
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id
+      js.src = '//connect.facebook.net/en_US/sdk.js'
+      fjs.parentNode.insertBefore(js, fjs)
+      }(document, 'script', 'facebook-jssdk'))
+    window.fbAsyncInit = () => {
+      FB.init({
+        appId            : '2274502912837375',
+        autoLogAppEvents : true,
+        xfbml            : true,
+        version          : 'v2.10'
+      })
+        FB.AppEvents.logPageView()
+      FB.getLoginStatus(res => {
+        const loginButton = document.querySelector('button')
+        if (res.status === 'connected') {
+          loginButton.disabled = true
+          this.grabData()
+        }
+      })
+  }
     platform.ready().then(() => {
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
+      statusBar.styleDefault()
+      splashScreen.hide()
+    })
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ReactiveFormsModule }   from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,7 +12,6 @@ import { ReactiveFormsModule }   from '@angular/forms';
     HomePage
   ],
   imports: [
-    ReactiveFormsModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
